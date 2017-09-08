7c436924dca4627c147dcd15776a4b23b92e5e42
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-09-09 08:40:08 +0900

    change environment variavle into .env.sample

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,10 +1,6 @@
 class WebhookController < ApplicationController
   protect_from_forgery with: :null_session
 
-  LINE_PRODUCTION_API_SECRET = ENV.fetch("LINE_PRODUCTION_API_SECRET")
-  OUTBOUND_PROXY = ENV.fetch("OUTBOUND_PROXY")
-  LINE_PRODUCTION_API_KEY = ENV.fetch("LINE_PRODUCTION_API_KEY")
-
   def callback
     unless is_validate_signature
       render :nothing => true, status: 470
@@ -45,7 +41,7 @@ class WebhookController < ApplicationController
       output_text = input_text
     end
 
-    client = LineClient.new(LINE_PRODUCTION_API_KEY, OUTBOUND_PROXY)
+    client = LineClient.new(ENV.fetch("LINE_PRODUCTION_API_KEY"), ENV.fetch("OUTBOUND_PROXY"))
     res = client.reply(replyToken, output_text)
 
     if res.status == 200
@@ -62,7 +58,7 @@ class WebhookController < ApplicationController
   def is_validate_signature
     signature = request.headers["X-LINE-Signature"]
     http_request_body = request.raw_post
-    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, LINE_PRODUCTION_API_SECRET, http_request_body)
+    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, ENV.fetch("LINE_PRODUCTION_API_SECRET"), http_request_body)
     signature_answer = Base64.strict_encode64(hash)
     signature == signature_answer
   end
